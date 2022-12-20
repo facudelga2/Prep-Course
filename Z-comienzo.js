b999fad2a3a910729bfe776c7c87b8d481a39a22
facudelga2 <88855752+facudelga2@users.noreply.github.com>
2022-12-19 23:29:21 -0300

    4 ejercicios realizados

---
+++ Z-comienzo.js
@@ -0,0 +1,24 @@
+var Mati = 'Gola'
+Mati 
+
+function presentar (nombre, edad) {
+    console.log('Hola, mi nombre es ' + nombre);
+    console.log('Tengo ' + edad + ' años.');
+}
+function suma(a, b){
+    console.log(a + b)
+}
+
+function Menejar(edad){
+    if(edad > 18) {
+        return true;
+    }
+    return false;
+
+}
+
+if(edad > 21) {
+    console.log("sos viejo");
+    } else{
+        console.log('sos joven')
+    }

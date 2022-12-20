31bb80ce95a9e38057c15c9e9386c4e25c294169
facudelga2 <88855752+facudelga2@users.noreply.github.com>
2022-12-20 05:10:07 -0300

    Tarea Completa JS 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -69,14 +69,16 @@ function sonIguales(x, y) {
 }
 
 function tienenMismaLongitud(str1, str2) {
-  
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código: 
  if (str1.length === str2.length) {
   return true;
  } else {
   return false;
  }
  
-} console.log (tienenMismaLongitud ("lol", "sol"))
+} // console.log (tienenMismaLongitud ("lol", "sol"))
 
 
 
@@ -84,128 +86,192 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90){
+    return true;
+  } else {
+    return false;
+  }
 }
+// console.log (menosQueNoventa(89))
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+//console.log (mayorQueCincuenta(5))
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  return (x % y);
   
 }
+// console.log (obtenerResto(4, 3))
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
+//console.log(esPar (5))
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 1){
+    return true
+    } else {
+    return false
+   }
 }
+// console.log (esImpar (12141))
+
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return Math.pow(num, 2);
 }
+//console.log (elevarAlCuadrado(6))
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+   return Math.pow(num, 3)
 }
+//console.log (elevarAlCubo(3))
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  return Math.pow (num, exponent);
   
 }
+//console.log(elevar(2, 3))
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return Math.round(num)
 }
+//console.log(redondearNumero(5.42))
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+
+var resto = num % 1;
+var entero = num - resto;
+
+if (resto >= 0.5) {
+ return entero + 1;}
+
+  else {
+  return entero;}
+
+  return Math.round(num);
 }
+// console.log (redondearHaciaArriba(4.0001))
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return Math.random() 
 }
+//console.log (numeroRandom())
 
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+
+  if(numero === 0) {
+    return false;
+  }
+  else if(numero > 0) {
+    return "Es positivo";
+  }
+  else {
+    return "Es negativo";
+  }
 }
+// console.log(esPositivo(5));
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
+return (str + "!")
+  
 }
+//console.log(agregarSimboloExclamacion("Hola puto de mrd"));
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return ( "Soy " + nombre + " " + apellido)
 }
+//console.log(combinarNombres("Facundo", "Delgado"));
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return ("Hola " + nombre + "!")
 }
+//console.log(obtenerSaludo("Facundo"));
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return (alto * ancho)
   
 }
-
+//console.log(obtenerAreaRectangulo(2,4));
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   
+  return (lado * 4)
 }
+//console.log(retornarPerimetro(2));
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
 
+  return (base * altura) / 2 
 }
+//console.log(areaDelTriangulo(5, 10));
 
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return (euro * 1.2)
 }
+
 
 
 function esVocal(letra){
@@ -215,9 +281,27 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   
+  
+
+//   if (letra === "a", "e", "i", "o", "u" ){
+//     return "Es vocal"
+//   } else if (letra.length > 1 ){
+//     return "Dato Incorrecto"
+//   } else {
+//     return "Dato incorrecto2"
+//   }
+// } 
+// console.log(esVocal("e"));
+if(letra.length > 1){
+  return "Dato incorrecto"
 }
+if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+  return "Es vocal"
+}
+return "Dato incorrecto"
 
-
+} 
+// console.log(esVocal("easgsag"))
 
 // No modificar nada debajo de esta línea
 // --------------------------------
